8a7ddbd9fa6242aeea1cf73ca98d13fe60ec26be
Raj Patel <rp94@njit.edu>
2026-08-03 20:37:54 +0530

    Refactor backend using controllers and routes

---
+++ server/controllers/taskController.js
@@ -0,0 +1,61 @@
+import Task from "../models/Task.js";
+
+export const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Failed to fetch tasks",
+    });
+  }
+};
+
+export const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Failed to create task",
+    });
+  }
+};
+
+export const updateTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!task) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Failed to update task",
+    });
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Failed to delete task",
+    });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,78 +1,17 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
-import Task from "./models/Task.js";
+import taskRoutes from "./routes/taskRoutes.js"
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-dotenv.config();
+
 connectDB();
 //Middleware between req.body and json request bodies
 app.use(express.json());
+app.use("/tasks", taskRoutes);
 
-//GET API
-app.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find();
-    res.json(tasks);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "Failed to fetch tasks",
-    });
-  }
-});
-
-//POST API
-app.post("/tasks", async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json(task);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "Failed to create task",
-    });
-  }
-});
-
-//PUT API(Edit)
-app.put("/tasks/:id", async (req, res) => {
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    if (!task) {
-      return res.status(404).json({
-        message: "Task not found",
-      });
-    }
-    res.status(200).json(task);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "Failed to edit task",
-    });
-  }
-});
-
-//DELETE API
-app.delete("/tasks/:id", async (req, res) => {
-  try {
-    const task = await Task.findByIdAndDelete(req.params.id);
-    if(!task){
-        return res.status(404).json({
-            message: "Task not found",
-        });
-    }
-    res.status(200).json(task);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "Failed to delete task",
-    });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on Port: ${PORT}`);

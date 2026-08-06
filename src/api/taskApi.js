0f29a607beddc907efd6d5f254e2ad5087bd16d0
Raj Patel <rp94@njit.edu>
2026-08-06 20:34:46 +0530

    Refactor API configuration using Vite environment variables

--- src/api/taskApi.js
+++ src/api/taskApi.js
@@ -1,5 +1,6 @@
-const BASE_URL = "http://localhost:5000/tasks"
 import axios from "axios";
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 
 //Fetch all tasks from the backend (GET)
 export async function getTasks() {
